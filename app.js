58810961c490ef58011bf019c2bbd6dfab03c9da
K-Vardhan <kritivardhan020@gmail.com>
2023-04-30 02:25:52 +0530

    Add payment modal

--- app.js
+++ app.js
@@ -92,15 +92,15 @@ menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
 
-        choosenProduct =  products[index]
-        
+        choosenProduct = products[index]
+
         currentProductTitle.textContent = choosenProduct.title;
 
         currentProductPrice.textContent = "$" + choosenProduct.price;
 
         currentProductImg.src = choosenProduct.colors[0].img
 
-        currentProductColors.forEach((color,index)=>{
+        currentProductColors.forEach((color, index) => {
             color.style.backgroundColor = choosenProduct.colors[index].code;
         });
     });
@@ -108,6 +108,17 @@ menuItems.forEach((item, index) => {
 
 currentProductColors.forEach((color, index) => {
     color.addEventListener("click", () => {
-      currentProductImg.src = choosenProduct.colors[index].img;
+        currentProductImg.src = choosenProduct.colors[index].img;
     });
-  });
+});
+
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach((size) => {
+            size.style.backgroundColor = "white";
+            size.style.color = "black";
+        });
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+}); 
